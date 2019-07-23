c09e3f8e2eeef689c8b249b920970e861cff3d4f
arc-arnob <52620983+arc-arnob@users.noreply.github.com>
2019-07-23 08:39:06 +0530

    Add files via upload

---
+++ MultipleLinearRegression.py
@@ -0,0 +1,81 @@
+# Multiple Linear Regression
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataSets
+dataset=pd.read_csv("50_Startups.csv")
+X= dataset.iloc[:,:-1].values
+y= dataset.iloc[:,4].values
+
+#Encoding Categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X= LabelEncoder()
+X[:,3]= labelencoder_X.fit_transform(X[:,3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X=onehotencoder.fit_transform(X).toarray()
+
+# Avoiding Dummy Variable Trap
+X=X[:,1:]
+
+#Splitting data in training and test Sets
+from sklearn.model_selection import train_test_split
+X_train , X_test , y_train , y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+
+#Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X= StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)"""
+
+#Fitting Multiple Linear Regression Model
+from sklearn.linear_model import LinearRegression
+regressor= LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting test Set
+y_pred=regressor.predict(X_test) 
+
+# Backward Elimination Method
+import statsmodels.formula.api as sm
+X=np.append(arr=np.ones((50,1)).astype(int),values=X,axis=1)
+X_opt= X[:, [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit() #step 2
+regressor_OLS.summary()
+X_opt= X[:, [0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit() #step 2
+regressor_OLS.summary()
+X_opt= X[:, [0,3,4,5]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit() #step 2
+regressor_OLS.summary()
+X_opt= X[:, [0,3,5]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit() #step 2
+regressor_OLS.summary()
+X_opt= X[:, [0,3]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit() #step 2
+regressor_OLS.summary()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
